1a00175b20887c6510c045feaf13663f89b8af56
Bala Chandrasekaran <mouli716@gmail.com>
2018-02-07 02:10:35 -0600

    finished DFS

--- MP1/maze_dfs.py
+++ MP1/maze_dfs.py
@@ -16,24 +16,14 @@ class DFSMazeSolver:
         self.find_dfs(self.maze.start, None)
         sol_arr = []
         sol_set = set()
-        end_point = self.maze.end
+        point = self.maze.end
         #for coord in self.solution.keys():
         #    self.maze.maze_array[coord[0]][coord[1]] = '.'
-        # while(end_point != self.maze.start):
-        #     print(end_point[0], end_point[1])
-        #     self.maze.maze_array[end_point[0]][end_point[1]] = '.'
-        #     end_point = self.solution[end_point[0]][end_point[1]]
-        #     print(end_point)
+        while(point != self.maze.start):
+            point = self.solution[point]
+            self.maze.maze_array[point[0]][point[1]] = '.'
+        self.maze.maze_array[self.maze.start[0]][self.maze.start[1]] = 'P'
         sol_arr.append(self.solution[self.maze.end])
-        #loop to add parent node
-        i =1
-        while sol_arr[i-1] != self.maze.start:
-            dict_index = sol_arr[i-1]
-            sol_arr.append(self.solution[dict_index])
-            sol_set.add(sol_arr[i])
-            i+=1
-        print()
-
         i,j = 0,0
         for item in self.maze.maze_array:
             for c in item:
@@ -45,7 +35,7 @@ class DFSMazeSolver:
             j = 0
             i+=1
             print()
-
+        # self.maze.maze_array[self.maze.start[0]][self.maze.start[1]] == 'P'
 
     def find_dfs(self, current, prev):
         self.counter += 1
@@ -61,11 +51,6 @@ class DFSMazeSolver:
             x,y = curr_point[0], curr_point[1]
             if(curr_point == self.maze.end):
                 return
-            if(self.maze.maze_array[x+1][y] != '%' and (x+1,y) not in self.visited):
-                search_path.append((x+1,y))
-                self.visited.add((x+1,y))
-                self.solution[(x+1,y)] = prev
-                curr_point = (x+1,y)
             if(self.maze.maze_array[x-1][y] != '%' and (x-1,y) not in self.visited):
                 search_path.append((x-1,y))
                 self.visited.add((x-1,y))
@@ -76,6 +61,11 @@ class DFSMazeSolver:
                 self.visited.add((x,y+1))
                 self.solution[(x,y+1)] = prev
                 curr_point = (x,y+1)
+            if(self.maze.maze_array[x+1][y] != '%' and (x+1,y) not in self.visited):
+                search_path.append((x+1,y))
+                self.visited.add((x+1,y))
+                self.solution[(x+1,y)] = prev
+                curr_point = (x+1,y)
             if(self.maze.maze_array[x][y-1] != '%' and (x,y-1) not in self.visited):
                 search_path.append((x,y-1))
                 self.visited.add((x,y-1))
@@ -95,16 +85,16 @@ dfs.solve_maze()
 t = time.clock() - t0
 print('it took this long ', t)
 
-# m = MazeLoader(MazeLoader.big)
-# dfs = DFSMazeSolver(m)
-# t0 = time.clock()
-# dfs.solve_maze()
-# t = time.clock() - t0
-# print('it took this long ', t)
-#
-# m = MazeLoader(MazeLoader.last)
-# dfs = DFSMazeSolver(m)
-# t0 = time.clock()
-# dfs.solve_maze()
-# t = time.clock() - t0
-# print('it took this long ', t)
+m = MazeLoader(MazeLoader.big)
+dfs = DFSMazeSolver(m)
+t0 = time.clock()
+dfs.solve_maze()
+t = time.clock() - t0
+print('it took this long ', t)
+
+m = MazeLoader(MazeLoader.last)
+dfs = DFSMazeSolver(m)
+t0 = time.clock()
+dfs.solve_maze()
+t = time.clock() - t0
+print('it took this long ', t)
