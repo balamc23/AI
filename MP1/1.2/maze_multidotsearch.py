eab2fbb78f98b3c61e8a1585bb4136d73924f864
ArkoBanik <banik2@illinois.edu>
2018-02-11 03:46:33 -0600

    Working multidot

--- MP1/1.2/maze_multidotsearch.py
+++ MP1/1.2/maze_multidotsearch.py
@@ -6,16 +6,17 @@ from operator import itemgetter
 import string
 
 class MultiDotSolver:
-	def __init__(self,maze):
-		if(maze is None):
-			print('Error: No maze is passed in')
-		self.maze = maze
-		self.visited = set()
-		self.solution = list()
-		self.steps = 0
-		self.counter = 0
+    def __init__(self,maze):
+        if(maze is None):
+            print('Error: No maze is passed in')
+        self.maze = maze
+        self.visited = set()
+        self.solution = list()
+        self.steps = 0
+        self.counter = 0
+        self.length = 0
 
-    def manhattan_distance(self, x, y):
+    def manhattan_distance(self, x,y):
         (x1, y1) = x
         (x2, y2) = y
         return abs(x1 - x2) + abs(y1 - y2)
@@ -25,8 +26,6 @@ class MultiDotSolver:
         # print('num of dots', self.maze.num_dots)
         self.multidot(self.maze, self.maze.start, self.maze.end)
         #point = self.maze.endpoints
-        self.solution.pop(0)
-        self.solution.pop(0)
         # print('solution', self.solution)
         for i in self.solution:
             (x,y) = i
@@ -37,13 +36,15 @@ class MultiDotSolver:
                 print(c, end= ' ')
             print()
         print('The number of nodes explored is: ', self.steps)
-        print('The number of steps taken is: ' , len(self.solution)-1)
+        print('The number of steps taken is: ' , self.length)
+
+
 
     def multidot(self, maze, start, end):
-    	print('dim', self.maze.width, self.maze.height)
+        #print('dim', self.maze.width, self.maze.height)
         alphabet = dict(zip(string.ascii_lowercase, range(1,27)))
         alpha = dict (zip(alphabet.values(),alphabet.keys()))
-        print('alpha' , alpha)
+        #print('alpha' , alpha)
         abcs = {}
         first = start
         final_order = []
@@ -53,61 +54,96 @@ class MultiDotSolver:
         nodes = 0
 
         while(done_check != self.maze.num_dots):
-        	d_ = []
-        	for point in loc_end:
-        		total_cost = MultiDotSolver.astar(maze,first,end_point)
-        		d_.append((total_cost, end_point))
-        	d_.sort(d_, key = lambda x: x[0])
-        	final_order.append(d_[0])
-        	first = loc_end.remove(d_[0])
+            d_ = []
+            for point in loc_end:
+                result = self.astar(maze,first,point)
+                total_cost = result[0]
+                curr_cost = result[1]
+                d_.append((total_cost, point,curr_cost))
+            d_ = sorted(d_, key = lambda x: x[0])
+            final_order.append(d_[0])
+            self.length += d_[0][2]
+            first =d_[0][1]
+            self.maze.maze_array[d_[0][1][0]][d_[0][1][1]] = alpha[done_check+1]
+            done_check +=1
 
 
     def astar(self, maze, start, end):
-        # print('end', end)
-        pq = []
-        heapq.heappush(pq, (self.manhattan_distance(start, end), start, 0, list(start)))
-        local_sol = []
+        #initializes all local variables needed
+        #set to check for already visited cells
+        visitited = set()
+        #solution dict
+        draw_solution = {start:start}
+        solution = []
+        solution_set = set()
+        # Add start to queue
+        ts = time.time()
+        #q = queue.Queue()
+        d = self.manhattan_distance(start,end)
+        total_cost = d + 0
+        q = [(total_cost, start,0)]
+        visitited = set()
+        maze_done  = 0
+        nodes = 0
+        
+        while(len(q) != 0):
+            curr_node= heapq.heappop(q)
+            curr_pos = curr_node[1]
+            curr_cost = curr_node[2]
+            self.steps +=1
+            neighbors = self.get_neighbors(curr_pos)
+            for pos in neighbors:
+                x, y = pos[0], pos[1]
+                if(self.maze.maze_array[x][y] != '%'):
+                    if(pos not in visitited):
+                        d = self.manhattan_distance(pos,end)
+                        total_cost = d + curr_cost + 1
+                        heapq.heappush(q,(total_cost,pos, curr_cost+1))
+                        #add pos to visited set
+                        draw_solution[pos] =curr_pos
+                        visitited.add(pos)
+                if(self.maze.maze_array[x][y] == "."):
+                    #print("found solution")
+                    d = self.manhattan_distance(pos,end)
+                    total_cost = d + curr_cost + 1
+                    ts2 = time.time()
+                    draw_solution[pos] =curr_pos
+                    maze_done =1
+                    return (total_cost, curr_cost+1)
+                    break
+            if(maze_done):
+                break
 
-        while(len(pq) != 0):
-            self.steps += 1
-            curr_point = heapq.heappop(pq)
-            curr_cost = curr_point[2] #taking third value, cost, of pq tuple
-            if curr_point[1] == end:
-                local_sol = deepcopy(curr_point[3])
-                local_sol.append((curr_point[1])) # adding current point to saved solution
-                print('Cost', curr_cost)
-                return curr_cost
-            x,y = curr_point[1][0], curr_point[1][1]
 
-            local_sol = deepcopy(curr_point[3])
-            local_sol.append((curr_point[1])) # adding current point to saved solution
-            self.visited.add(curr_point[1]) # add tuple to visited
-            # self.printer(self.solution)
-            # time.sleep(0.05)
-            if(self.maze.maze_array[x-1][y] != '%' and (x-1,y) not in self.visited):
-                d = self.manhattan_distance((x-1,y), end) + curr_cost + 1
-                curr_sol_1 = deepcopy(self.solution)
-                temp_tuple_1 = (d, (x-1, y), curr_cost + 1, curr_sol_1)
-                heapq.heappush(pq, temp_tuple_1)
-            if(self.maze.maze_array[x+1][y] != '%' and (x+1,y) not in self.visited):
-                d = self.manhattan_distance((x+1,y), end) + curr_cost + 1
-                curr_sol_2 = deepcopy(self.solution)
-                temp_tuple_2 = (d, (x+1, y), curr_cost + 1, curr_sol_2)
-                heapq.heappush(pq, temp_tuple_2)
-            if(self.maze.maze_array[x][y+1] != '%' and (x,y+1) not in self.visited):
-                d = self.manhattan_distance((x,y+1), end) + curr_cost + 1
-                curr_sol_3 = deepcopy(self.solution)
-                temp_tuple_3 = (d, (x, y+1), curr_cost + 1, curr_sol_3)
-                heapq.heappush(pq, temp_tuple_3)
-            if(self.maze.maze_array[x][y-1] != '%' and (x,y-1) not in self.visited):
-                d = self.manhattan_distance((x,y-1), end) + curr_cost + 1
-                curr_sol_4 = deepcopy(self.solution)
-                temp_tuple_4 = (d, (x, y-1), curr_cost + 1, curr_sol_4)
-                heapq.heappush(pq, temp_tuple_4)
-        # From start find total cost( M_dist + path_cost + points left) of every end point
-        # Priotize largest man dist when checking each one
-        # Take smallest total cost as point to go to first
-        # From there do the exact same thing but do it from the end point you just went to
-        # Boom thats it 
-        # Time to code this shit
+    def get_neighbors(self, cell):
+        x, y = cell[0], cell[1]
+        return [
+            (x + 1, y),
+            (x - 1, y),
+            (x, y + 1),
+            (x, y - 1),
+        ]
+
+
+
+m = MazeLoader('mazes_suboptimal/tinySearch.txt')
+astar = MultiDotSolver(m)
+t0 = time.clock()
+astar.solve_maze()
+t = time.clock() - t0
+print('it took this long ', t)
+
+m = MazeLoader('mazes_suboptimal/smallSearch.txt')
+astar = MultiDotSolver(m)
+t0 = time.clock()
+astar.solve_maze()
+t = time.clock() - t0
+print('it took this long ', t)
+
+m = MazeLoader('mazes_suboptimal/mediumSearch.txt')
+astar = MultiDotSolver(m)
+t0 = time.clock()
+astar.solve_maze()
+t = time.clock() - t0
+print('it took this long ', t)
 
