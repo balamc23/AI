f1b524153c367d84184f7e1afa1928cce13fb766
ArkoBanik <banik2@illinois.edu>
2018-02-10 22:54:10 -0600

    pls fix indents ty

--- MP1/1.2/maze_multidotsearch.py
+++ MP1/1.2/maze_multidotsearch.py
@@ -8,7 +8,7 @@ import string
 class MultiDotSolver:
 
 	 def __init__(self, maze):
-        if(maze is None):
+     	if(maze is None):
             print('Error: No maze passed in')
         self.maze = maze
         self.visited = set()
@@ -47,11 +47,68 @@ class MultiDotSolver:
         print('alpha' , alpha)
         abcs = {}
         first = start
-        d_ = []
+        final_order = []
+        done_check = 0
+        first = start
+        loc_end = deepcopy(end)
+        nodes = 0
 
+        while(done_check != self.maze.num_dots):
+        	d_ = []
+        	for point in loc_end:
+        		total_cost = MultiDotSolver.astar(maze,first,end_point)
+        		d_.append((total_cost, end_point))
+        	d_.sort(d_, key = lambda x: x[0])
+        	final_order.append(d_[0])
+        	first = loc_end.remove(d_[0])
+
+
+    def astar(self, maze, start, end):
+        # print('end', end)
+        pq = []
+        heapq.heappush(pq, (self.manhattan_distance(start, end), start, 0, list(start)))
+        local_sol = []
+
+        while(len(pq) != 0):
+            self.steps += 1
+            curr_point = heapq.heappop(pq)
+            curr_cost = curr_point[2] #taking third value, cost, of pq tuple
+            if curr_point[1] == end:
+                local_sol = deepcopy(curr_point[3])
+                local_sol.append((curr_point[1])) # adding current point to saved solution
+                print('Cost', curr_cost)
+                return curr_cost
+            x,y = curr_point[1][0], curr_point[1][1]
+
+            local_sol = deepcopy(curr_point[3])
+            local_sol.append((curr_point[1])) # adding current point to saved solution
+            self.visited.add(curr_point[1]) # add tuple to visited
+            # self.printer(self.solution)
+            # time.sleep(0.05)
+            if(self.maze.maze_array[x-1][y] != '%' and (x-1,y) not in self.visited):
+                d = self.manhattan_distance((x-1,y), end) + curr_cost + 1
+                curr_sol_1 = deepcopy(self.solution)
+                temp_tuple_1 = (d, (x-1, y), curr_cost + 1, curr_sol_1)
+                heapq.heappush(pq, temp_tuple_1)
+            if(self.maze.maze_array[x+1][y] != '%' and (x+1,y) not in self.visited):
+                d = self.manhattan_distance((x+1,y), end) + curr_cost + 1
+                curr_sol_2 = deepcopy(self.solution)
+                temp_tuple_2 = (d, (x+1, y), curr_cost + 1, curr_sol_2)
+                heapq.heappush(pq, temp_tuple_2)
+            if(self.maze.maze_array[x][y+1] != '%' and (x,y+1) not in self.visited):
+                d = self.manhattan_distance((x,y+1), end) + curr_cost + 1
+                curr_sol_3 = deepcopy(self.solution)
+                temp_tuple_3 = (d, (x, y+1), curr_cost + 1, curr_sol_3)
+                heapq.heappush(pq, temp_tuple_3)
+            if(self.maze.maze_array[x][y-1] != '%' and (x,y-1) not in self.visited):
+                d = self.manhattan_distance((x,y-1), end) + curr_cost + 1
+                curr_sol_4 = deepcopy(self.solution)
+                temp_tuple_4 = (d, (x, y-1), curr_cost + 1, curr_sol_4)
+                heapq.heappush(pq, temp_tuple_4)
         # From start find total cost( M_dist + path_cost + points left) of every end point
         # Priotize largest man dist when checking each one
         # Take smallest total cost as point to go to first
         # From there do the exact same thing but do it from the end point you just went to
         # Boom thats it 
         # Time to code this shit
+
