5c5a43719abb179a8047dd36146b2a310d23500f
ArkoBanik <banik2@illinois.edu>
2018-04-27 05:00:26 -0500

    Basic functionality, need to test further

--- MP4/QLearn.py
+++ MP4/QLearn.py
@@ -1,21 +1,78 @@
 import numpy as np 
 import math
 from mdp_env import MarkovDecisionProcess as MDP
+from progress import printProgressBar
+from time import sleep
+
 
 Num_States = 10369	# 12*12*3*2*12 +1<---  how to get num states
+alpha = 0.2
+gamma = 0.2
 
 
 class QLearn:
 
 	def  __init__(self):
 		self.Qtable = {}
+		self.mdp = MDP()
 		for x_pos in range(12):
 			for y_pos in range(12):
-				for x_vel in range(2):
-					for y_vel in range(3):
+				for x_vel in (-1,1):
+					for y_vel in (-1,0,1):
 						for paddle_y in range(12):
 							self.Qtable[(x_pos,y_pos,x_vel,y_vel,paddle_y)] = [0,0,0]
 
-		
+	def play(self,train):
+		score = 0
+		R = 0
+		while(R != -1):
+
+			state = self.mdp.get_d_state()
+
+			action = self.Qtable[state].index(max(self.Qtable[state]))
+
+			R = self.mdp.update_environment(action)
+
+			new_state = self.mdp.get_d_state()
+
+			if(train == 1):
+				x = self.Qtable[state][action]
+				#misses paddle
+				if(R < 0):
+					x_next = 0			# note sure if setting this to 0 is the right move
+				#doesnt lose
+				else:
+					x_next = max(self.Qtable[new_state])
+				self.Qtable[state][action] = x + alpha* (R + gamma*x_next - x)
+
+			score += R
+
+		return score
+
+
+	def train(self):
+		Iterations = 50000
+		printProgressBar(0, Iterations, prefix = 'Training:', suffix = 'Complete', length = 50)
+		for i in range(Iterations):
+			# print('iteration: ',i)
+			s = self.play(1)
+			self.mdp.init_envvironment()
+			printProgressBar(i + 1, Iterations, prefix = 'Training:', suffix = 'Complete', length = 50)
+
+
+
+	def test(self):
+		for i in range(6):
+			s = self.play(0)
+			print('game', i, 'score: ' , s)
+			self.mdp.init_envvironment()
+
+
+Q = QLearn()
+Q.train()
+Q.test()
+
+
+
 
 
--- MP4/mdp_env.py
+++ MP4/mdp_env.py
@@ -12,7 +12,6 @@ class MarkovDecisionProcess:
 		self.paddle_y = None
 		self.paddle_height = 0.2
 
-
 		self.init_envvironment()
 
 	def init_envvironment(self):
@@ -33,49 +32,58 @@ class MarkovDecisionProcess:
 		self.ball_y += self.velocity_y
 
 		self.paddle_y += self.actions[action]
-
+		if(self.paddle_y < 0):
+			self.paddle_y = 0
 
 
 		if(self.ball_y < 0):
 			self.ball_y = -self.ball_y
 			self.velocity_y = -self.velocity_y
-			return	R
+			# return	R
 		if(self.ball_y > 1):
 			self.ball_y = 2 - self.ball_y
 			self.velocity_y = -self.velocity_y
-			return R
+			# return R
 		if(self.ball_x < 0):
 			self.ball_x = -self.ball_x
 			self.velocity_x = -self.velocity_x	
-			return R
-		if(self.ball_x == 1 and (self.ball_y >= self.paddle_y and self.ball_y <= self.paddle_y + self.paddle_height)):
+			# return R
+		if(self.ball_x > 1 and (self.ball_y >= self.paddle_y and self.ball_y <= self.paddle_y + self.paddle_height)):
 			R = 1
 			self.ball_x = 2 *1 - self.ball_x
 			if(abs(self.velocity_x + U) < 1):
 				self.velocity_x = -self.velocity_x + U
+			else:
+				self.velocity_x = -self.velocity_x
 			if(abs(self.velocity_y + V) <1):
 				self.velocity_y = self.velocity_y + V
-			return R
-		else:
+			# return R
+		if(self.ball_x > 1 and (self.ball_y <= self.paddle_y or self.ball_y >= self.paddle_y + self.paddle_height)):
 			R = -1
-			return R
+			# return R
+
+		
+		return R
 
 
 	def get_d_state(self):
-		d_x_pos = math.floor(self.ball_x)
-		d_y_pos = math.floor(self.ball_y)
+		d_x_pos = math.floor(12*self.ball_x)
+		d_y_pos = math.floor(12*self.ball_y)
 		d_x_vel = 1
 		if(self.velocity_x < 0):
 			d_x_vel = -1
 		d_y_vel = 0
-		if(self.velocity_y > 0):
+		if(self.velocity_y > 0.015):
 			d_y_vel = 1
-		if(self.velocity_y < 0):
+		if(self.velocity_y < -0.015):
 			d_y_vel = -1
 
 		d_paddle = math.floor(12 * self.paddle_y / (1-self.paddle_height))
-		if(self.paddle_y == 1-self.paddle_height):
+		# if(self.paddle_y == 1-self.paddle_height):
+		if(d_paddle > 11):		# not sure if this is ok
 			d_paddle = 11
+
+		return (d_x_pos,d_y_pos,d_x_vel,d_y_vel,d_paddle)
 
 
 
