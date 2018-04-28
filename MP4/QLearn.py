8c0d43305f205b4f482604ca0cd4ad2e25113a25
ArkoBanik <banik2@illinois.edu>
2018-04-28 01:00:40 -0500

    1.2 basics working

--- MP4/QLearn.py
+++ MP4/QLearn.py
@@ -1,8 +1,10 @@
 import numpy as np 
 import math
 from mdp_env import MarkovDecisionProcess as MDP
+from mdp_env_ec import MarkovDecisionProcess_EC as MDP_EC
 from progress import printProgressBar
 from time import sleep
+import copy
 
 
 Num_States = 10369	# 12*12*3*2*12 +1<---  how to get num states
@@ -14,7 +16,9 @@ class QLearn:
 
 	def  __init__(self):
 		self.Qtable = {}
+		self.Qtable_ec = {}
 		self.mdp = MDP()
+		self.mdp_ec = MDP_EC()
 		for x_pos in range(12):
 			for y_pos in range(12):
 				for x_vel in (-1,1):
@@ -49,16 +53,49 @@ class QLearn:
 
 		return score
 
+	def play_ec(self,train):
+		score = 0
+		R = 0
+		while(R != -1):
+
+			state = self.mdp_ec.get_d_state()
+
+			action = self.Qtable_ec[state].index(max(self.Qtable_ec[state]))
+
+			R = self.mdp_ec.update_environment(action)
+
+			new_state = self.mdp_ec.get_d_state()
+
+			if(train == 1):
+				x = self.Qtable_ec[state][action]
+				#misses paddle
+				if(R < 0):
+					x_next = 0			# note sure if setting this to 0 is the right move
+				#doesnt lose
+				else:
+					x_next = max(self.Qtable_ec[new_state])
+				self.Qtable_ec[state][action] = x + alpha* (R + gamma*x_next - x)
+
+			score += R
+
+		return score
 
 	def train(self):
 		Iterations = 50000
 		printProgressBar(0, Iterations, prefix = 'Training:', suffix = 'Complete', length = 50)
 		for i in range(Iterations):
-			# print('iteration: ',i)
 			s = self.play(1)
 			self.mdp.init_envvironment()
 			printProgressBar(i + 1, Iterations, prefix = 'Training:', suffix = 'Complete', length = 50)
 
+	def train_ec(self):
+		self.Qtable_ec = copy.deepcopy(self.Qtable)
+		Iterations = 50000
+		printProgressBar(0, Iterations, prefix = 'Training:', suffix = 'Complete', length = 50)
+		for i in range(Iterations):
+			s = self.play_ec(1)
+			self.mdp_ec.init_envvironment()
+			printProgressBar(i + 1, Iterations, prefix = 'Training:', suffix = 'Complete', length = 50)
 
 	def test(self):
 		num_success = 0
@@ -71,10 +108,24 @@ class QLearn:
 
 		print('number of successful games: ', num_success)
 
+	def test_ec(self):
+		num_success = 0
+		for i in range(200):
+			s = self.play_ec(0)
+			if (s >= 9):
+				print('game', i, 'score: ' , s)
+				num_success += 1
+			self.mdp_ec.init_envvironment()
+
+		print('number of successful games: ', num_success)
+
+	
 
 Q = QLearn()
 Q.train()
 Q.test()
+Q.train_ec()
+Q.test_ec()
 
 
 
