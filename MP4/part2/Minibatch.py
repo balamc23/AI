f538bbc3b019acd3b4d4619cd3b7aef880165f15
Bala Chandrasekaran <mouli716@gmail.com>
2018-04-29 13:08:21 -0500

    finished Mininbatch pseudo

--- MP4/part2/Minibatch.py
+++ MP4/part2/Minibatch.py
@@ -13,9 +13,6 @@ class DeepPong:
 		self.b3. = np.zeros(256)
 		self.b4. = np.zeros(3)
 
-
-
-
 	def readData(self,dataFile):
 		with open(dataFile) as df:
 			data = df.readlines()
@@ -28,9 +25,7 @@ class DeepPong:
 		A2,rcache2= ReLu_Forward(Z2)
 		Z3,acache3 = h.Affine_Forward(A2,W3,b3)
 		A3,rcache3 = ReLu_Forward(Z3)
-		F,acache4 = h.Affine_Forward(A3,W4,b4)
-		
-
+		F,acache4 = h.Affine_Forward(A3,W4,b4)		
 
 	def MiniBatchGD(self,Epochs):
 		# self.data = np.array(self.data)
@@ -38,16 +33,15 @@ class DeepPong:
 		n = 2000
 		for e in range(Epochs):
 			self.data = np.shuffle(self.data)
-			A = []
-			y = []
-			for i in self.data:				
-				A.append(i[:4])
-				y.append([5])
-
-			A = np.array(A)
-			y = np.array(y)
 			for i in range(N//n):
+				A = []
+				y = []
+				for i in self.data:				
+					A.append(i[:4])
+					y.append([5])
+				A = np.array(A)
+				y = np.array(y)				
 
-
+				loss = FourLayerNetwork(A, (self.W1, self.W2, self.W3, self.W4), (self.b1, self.b2, self.b3, self.b4), y, false)
 
 
