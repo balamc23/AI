d95f08334598e11013e2f14e050528e57c37fd31
ArkoBanik <banik2@illinois.edu>
2018-04-30 02:43:30 -0500

    fixed and tested helpers

--- MP4/part2/Minibatch.py
+++ MP4/part2/Minibatch.py
@@ -1,22 +1,38 @@
 import numpy as np 
 import helpers as h
+from progress import printProgressBar
+
 
 class DeepPong:
 	def __init__(self):
-		self.data = np.array((readData('expert_policy.txt')))
-		self.W1 = np.random.rand((5,256))
-		self.W2 = np.random.rand((256,256))
-		self.W3 = np.random.rand((256,256))
-		self.W4 = np.random.rand((256,3))
-		self.b1. = np.zeros(256)
-		self.b2. = np.zeros(256)		
-		self.b3. = np.zeros(256)
-		self.b4. = np.zeros(3)
+		self.data = np.array((self.readData('expert_policy.txt')))
+		self.W1 = np.random.rand(5,256)
+		self.W2 = np.random.rand(256,256)
+		self.W3 = np.random.rand(256,256)
+		self.W4 = np.random.rand(256,3)
+		self.b1 = np.zeros(256)
+		self.b2 = np.zeros(256)		
+		self.b3 = np.zeros(256)
+		self.b4 = np.zeros(3)
 		self.learning_rate = 0.1
 
 	def readData(self,dataFile):
 		with open(dataFile) as df:
-			data = df.readlines()
+			d_r = df.readlines()
+			data = np.zeros((len(d_r), 6))
+			i =0
+			for line in d_r:
+				string =''
+				j = 0	
+				for h in range(len(line)):
+					if(line[h] == ' ' or line[h] == '\n'):
+						data[i][j] = float(string)
+						string =''
+						j += 1
+					else:
+						string += line[h]
+				i += 1
+				
 		return data	
 
 	def FourLayerNetwork(self,X,W1,W2,W3,W4,b1,b2,b3,b4,y,test,n):
@@ -32,7 +48,7 @@ class DeepPong:
 			classification = []
 			return classification
 
-		loss, dF = h.Cross_Entropy(F, y)
+		loss, dF = h.Cross_Entropy(F, y, n)
 		dA3, dW4, dB4 = h.Affine_Backward(dF, acache4)
 		dZ3 = h.ReLu_Backward(dA3, rcache3)
 		dA2, dW3, dB3 = h.Affine_Backward(dZ3, acache3)
@@ -58,17 +74,21 @@ class DeepPong:
 		# self.data = np.array(self.data)
 		N = 10000
 		n = 2000
+		printProgressBar(0, Epochs, prefix = 'Training:', suffix = 'Complete', length = 50)
 		for e in range(Epochs):
-			self.data = np.shuffle(self.data)
+			np.random.shuffle(self.data)
+			printProgressBar(e, Epochs, prefix = 'Training:', suffix = 'Complete', length = 50)
 			for j in range(0,N//n):
 				A = []
 				y = []
 				for i in range(j*n,j*n+n):				
-					A.append(self.data[i][:4])
+					A.append(self.data[i][:5])
 					y.append(self.data[i][5])
 				A = np.array(A)
 				y = np.array(y)				
 
-				loss = FourLayerNetwork(A, self.W1, self.W2, self.W3, self.W4, self.b1, self.b2, self.b3, self.b4, y, false,n)
+				loss = self.FourLayerNetwork(A, self.W1, self.W2, self.W3, self.W4, self.b1, self.b2, self.b3, self.b4, y, 0,n)
 
 
+DP = DeepPong()
+DP.MiniBatchGD(5)
